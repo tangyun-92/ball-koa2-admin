ddbaab0ebbdeb1b21e63471aa9a412c92c514c89
唐云 <tangyun1992@qq.com>
2021-09-02 11:00:08 +0800

    修改数据库连接配置

--- app/config/db.js
+++ app/config/db.js
@@ -2,7 +2,7 @@
  * @Author: 唐云 
  * @Date: 2021-07-25 21:50:33 
  * @Last Modified by: 唐云
- * @Last Modified time: 2021-09-02 10:45:45
+ * @Last Modified time: 2021-09-02 10:58:35
  */
 let MYSQL_CONF
 
@@ -16,7 +16,7 @@ MYSQL_CONF = {
 }
 
 BASE_CONFIG = {
-  staticUrl: '172.16.11.229:8080' // 静态资源地址 -- 存放图片
+  // staticUrl: '172.16.11.229:8080' // 静态资源地址 -- 存放图片
 }
 
 module.exports = {
